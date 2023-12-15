c6565b6e7fb7b70a80310c4daa6f324de54283b3
GT <55749965+gtiralongo@users.noreply.github.com>
2023-12-15 04:10:47 -0300

    Update settings.py

--- tasks_manager/settings.py
+++ tasks_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-1ax9$m*xm23+4tezr51h&=udjagf+63wty&e=fq*k8v0uphlp(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["anderson23508.pythonanywhere.com","127.0.0.1"]
+ALLOWED_HOSTS = ["codingmeet23508.pythonanywhere.com"]
 
 # será que es con '127.0.0.1'
 
